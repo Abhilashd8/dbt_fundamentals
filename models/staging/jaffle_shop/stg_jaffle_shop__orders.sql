693a2bf600e4940da482bcb7b77fca5f200acfad
Abhilash Dubasi <abhilashdubasi314@gmail.com>
2025-01-03 04:53:25 +0000

    commit

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -4,11 +4,23 @@
     )
 }}
 
-select
+with 
+
+orders as (
+    select * from {{ source('jaffle_shop', 'orders') }}
+),
+
+final as (select
     id as order_id,
     user_id as customer_id,
     order_date,
     status,
-    current_timestamp() as _etl_loaded_at
+    current_timestamp() as _etl_loaded_at,
+    row_number() over (
+    partition by user_id order by order_date, id
+    ) as user_order_seq
 
-from {{ source('jaffle_shop', 'orders') }}
+from orders 
+)
+
+select * from final
